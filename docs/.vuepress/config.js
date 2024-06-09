0c866c38b7cc4f520c4133f9ddbc671f77a3f459
Your Name <you@example.com>
2024-06-09 13:19:38 +0800

    test github action

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base: '/<REPO>/',
+  base: '/devtool-doc/',
   lang: 'en-US',
 
   title: 'VuePress',
